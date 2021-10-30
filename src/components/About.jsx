7d8eb47465fac9af4f4ac1e57294a6a9231c6734
Sashi445 <sashidhar652@gmail.com>
2021-10-30 13:09:23 +0530

    Added about!!

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,8 @@ const About = () => {
     return (<div className="container my-4">
         <h3>About</h3>
         <p>
-            getmygitignore
+            <strong>getmygitignore</strong> means exactly what it sounds!. All you have to do is pick a template and the gitignore
+            for that project will be generated. getmygitignore uses github API under the hood.
         </p>
     </div>);
 }
